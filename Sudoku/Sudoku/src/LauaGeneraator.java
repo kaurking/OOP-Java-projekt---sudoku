d727d65569d9ecd1a21c534ab9aef678e9f82d49
Kaur Kingsepp <kaurking@ut.ee>
2025-04-02 11:39:49 +0300

    suva comment

--- Sudoku/Sudoku/src/LauaGeneraator.java
+++ Sudoku/Sudoku/src/LauaGeneraator.java
@@ -1,5 +1,7 @@
 public class LauaGeneraator {
 
+    // mitteEemaldatudNumbrid nagu raskustase, mida rohkem jätad alles numbreid, seda lihtsam on
+    // Genereerid alguses valiidse laua ja siis eemaldad numbreid, Siis peab kontrollima, kas on lahendatav?
     public static SudokuLaud generate(int mitteEemaldatudNumbrid) {
         SudokuLaud fullBoard = generateFullBoard();
         removeCells(fullBoard, mitteEemaldatudNumbrid);
--- Sudoku/Sudoku/src/SudokuLaud.java
+++ Sudoku/Sudoku/src/SudokuLaud.java
@@ -1,7 +1,7 @@
 public class SudokuLaud {
 
     private SudokuRuut[][] laud;
-    
+
     public boolean makeMove(int rida, int veerg, int value) {return true;}
     public boolean isValidMove(int rida, int veerg, int value) {return true;}
     public boolean isComplete() {return true;}
